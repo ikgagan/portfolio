1f9799b13cab764f5fa902bc6e6ba498737cfeee
ikgagan <ik.gagan@gmail.com>
2024-10-17 22:48:14 -0400

    experience added

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -29,20 +29,19 @@ const Experience = () => {
                 Software Developer
                 </h1>
                 <span className=" text-[.9rem] font-semibold text-gray-500 sm:text-base">
-                 <a href="https://www.njit.edu/" target="_blank">Rebecca Everlene</a> 
+                 <a href="https://www.rebeccaeverlene.org/" target="_blank">Rebecca Everlene</a> 
                 </span>
                 <span className=" text-[.9rem] font-semibold text-cyan-500 sm:text-base">
                   Sept 2024 - Present
                 </span>
-                {/* <p className=" text-[.9rem] text-justify text-gray-500">
-                -> Successfully evolved software into a stable version with robust drag-and-drop capabilities and stepwise execution, ensuring reliable function selection and execution of data transformation. Implemented breakpoints, resulting in a 30% increase in debugging efficiency.<br/>
-                -> Successfully integrated and prepared over 50 data sets relevant to New Jersey (local political campaign contributions, township budgets, property taxes, U.S. Census, Covid-19 data, and climate data for analysis, ensuring data quality and reducing data inconsistencies by 25%.<br/>
-                -> Developed custom functions for data reconciliation, including case conversion, field separation, and value transformations, which improved data processing speed by 20%.<br/>
-                -> Implemented machine learning models like Regressions, Decision Tree and clustering to provide predictive analytics, improving data driven decision-making by 25%.<br/>
-                -> Presented query results and analyses in graphical formats, resulting in a 25% increase in user comprehension and engagement.<br/>
-                -> Designed a user-friendly interface for multi-dataset querying and analysis, enhancing user experience and query speed by 20%.<br/>
-                -> Demonstrated proficiency in a range of technologies like ReactJS, Django, HTML, CSS, JavaScript, Python, SQL, NoSQL, and API integration, resulting in a 50% reduction in development time for new features.<br/>
-                </p> */}
+                <p className=" text-[.9rem] text-justify text-gray-500">
+                <b> Business Vertical: EdTech. </b><br/>
+                -> Designed and developed user-friendly web interfaces for the MEDKids app, improving accessibility and increasing user engagement by 40% among young learners and educators. <br/>
+                -> Collaborated with a multidisciplinary team to implement interactive features such as virtual field trips and educational games, resulting in a 30% improvement in user interaction and learning outcomes.<br/>
+                -> Streamlined content integration processes, reducing content upload time by 25% and improving app performance and content delivery efficiency by 35%.<br/>
+                -> Optimized the app’s frontend performance, cutting page load times by 20%, leading to a 15% increase in overall user satisfaction.<br/>
+                -> Implemented feedback loops and iterative improvements, enhancing the app's functionality by 30% based on continuous user testing and stakeholder input. <br/>
+                </p>
               </div>
             </div>
           </fieldset>
@@ -71,6 +70,7 @@ const Experience = () => {
                   Aug 2023 - May 2024
                 </span>
                 <p className=" text-[.9rem] text-justify text-gray-500">
+                <b> Business Vertical: Information Technology. </b><br/>
                 -> Successfully evolved software into a stable version with robust drag-and-drop capabilities and stepwise execution, ensuring reliable function selection and execution of data transformation. Implemented breakpoints, resulting in a 30% increase in debugging efficiency.<br/>
                 -> Successfully integrated and prepared over 50 data sets relevant to New Jersey (local political campaign contributions, township budgets, property taxes, U.S. Census, Covid-19 data, and climate data for analysis, ensuring data quality and reducing data inconsistencies by 25%.<br/>
                 -> Developed custom functions for data reconciliation, including case conversion, field separation, and value transformations, which improved data processing speed by 20%.<br/>
@@ -154,6 +154,41 @@ const Experience = () => {
                 -> Implemented complex state management and dynamic form validations within the Workday system, ensuring seamless data entry and retrieval for over 1,000 employee records.<br/>
                 -> Leveraged React's component lifecycle and hooks to create modular, reusable components, reducing code redundancy by 25% and improving the maintainability of the application.<br/>
                 -> Integrated DevExpress and AG Grid within the Workday application, enabling advanced, customizable data grids that improved data processing speed by 40% and reduced reporting time by 35% for large datasets.<br/>
+                </p>
+              </div>
+            </div>
+          </fieldset>
+        </div>
+        <div className="right flex-1 flex items-center">
+          <fieldset
+            data-aos="zoom-in"
+            className=" w-[80%] p-5 py-12 sm:py-8 sm:w-full sm:p-2"
+          >
+            <div className=" relative">
+              {/* design */}
+              <div className="design flex absolute left-[-150px] top-1/2 items-center rotate-[90deg] sm:left-[-160px] ">
+                <div className="c1 w-[12px] h-[12px] rounded-full bg-white border-2 border-cyan-500"></div>
+                <div className="line w-[230px] bg-gray-300 h-[2px] sm:w-[250px]"></div>
+                <div className="c2 w-[12px] h-[12px] rounded-full bg-white border-2 border-cyan-500"></div>
+              </div>
+              {/* design */}
+              <div className=" flex flex-col gap-1 border-2 border-cyan-400 shadow-[0px_0px_16px_1px_rgba(0,0,0,0.1)] p-3 rounded-lg">
+                <h1 className="text-[1.4rem] font-semibold sm:text-xl">
+                Software Developer Intern
+                </h1>
+                <span className=" text-[.9rem] font-semibold text-gray-500 sm:text-base">
+                 <a>Entrepreneurship Garage</a> 
+                </span>
+                <span className=" text-[.9rem] font-semibold text-cyan-500 sm:text-base">
+                  October 2018 - November 2018
+                </span>
+                <p className=" text-[.9rem] text-justify text-gray-500">
+                <b> Business Vertical: EdTech. </b><br/>
+                -> Assisted a development team of 5 engineers, contributing to daily coding tasks, bug fixes, and feature enhancements while gaining experience in software project management and teamwork.<br/>
+                -> Supported team collaboration by participating in agile sprints, helping to streamline workflow, and improving code integration processes, leading to a 10% reduction in development time.<br/>
+                -> Contributed to the implementation of software solutions, focusing on system optimization and enhancing user experience, which resulted in a 5% improvement in application performance.<br/>
+                -> Worked closely with senior developers and industry experts, gathering technical insights to help refine development processes and apply best coding practices.<br/>
+                -> Helped meet project deadlines by supporting code testing, documentation, and deployment, contributing to smoother releases and improving delivery speed by 10%.<br/>
                 </p>
               </div>
             </div>
